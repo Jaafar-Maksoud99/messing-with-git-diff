01ae4ce0a23430b55e7fc68a334ca4feded602e5
Jaafar Maksoud <jaafar.maksoud@marketviewedu.com>
2024-01-10 15:55:54 -0500

    renamed python file

--- notebook-beployment-script.sh
+++ notebook-beployment-script.sh
@@ -1,2 +1,2 @@
-file_list=$(git diff --name-status HEAD~1 HEAD | awk 'BEGIN { FS="^[A-Z]+[ \t]+" } !(/^D/ || /^R/) && (/\.py$/) { print "\"" $2 "\"" }')
+file_list=$(git diff --name-status HEAD~1 HEAD | awk '(/\.py$/) { print $1 , "\"" $2 "\"" , $3 }')
 echo "$file_list"
